a0d7574ee26f413791e2e75bf38038bcccb9bf44
JavaScript <javascript@Admins-iMac-82.local>
2020-03-10 19:59:07 +0200

    gear

---
+++ exercitii/parrallax/gear.js
@@ -0,0 +1,13 @@
+document
+    .querySelector('.container')
+    .addEventListener('scroll', rotate);
+
+function rotate() {
+    let distance = document
+                .querySelector('.container')
+                .scrollTop;
+    document
+            .querySelector('.gear')
+            .style
+            .transform = `rotate(${distance}deg)`;
+}
