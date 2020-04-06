69155030dac1b17619d016a9c7439b4de0d4fb79
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-06 17:16:03 +0300

    X & 0 complete

--- exercitii/jquery/X si 0/xz.js
+++ exercitii/jquery/X si 0/xz.js
@@ -2,13 +2,17 @@ var activePlayer = 'X';
 var win = 0;
 var complet = 9;
 
+var nrJocX = 0;
+var nrJoc0 = 0;
+
+$('.winNrX').text('0');
+$('.winNr0').text('0');
+
 $('.table div').click(makeMove);
 
 function makeMove() {
     if (win == 0) {
         complet = complet - 1;
-        console.log(complet);
-        console.log(win);
         $(this).text(activePlayer)
             .addClass(activePlayer == 'X' ? 'x' : 'z')
             .unbind('click', makeMove);
@@ -32,7 +36,7 @@ function checkEndGame() {
     checkComplete(cells.filter((value, index) => index % 4 == 0));
 
     checkComplete(cells.filter((value, index) => [2, 4, 6].includes(index)));
-
+    finish();
 }
 
 /**
@@ -43,17 +47,30 @@ function checkComplete(cells) {
     if (cells.every(cell => $(cell).text() == 'X')
         || cells.every(cell => $(cell).text() == '0') && (complet != 0)
     ) {
-        $('.end').addClass('visible');
         win = 1;
-        console.log(win);
     }
+}
 
+function finish() {
+    if (win == 1) {
+        $('.end').addClass('visible').text((complet % 2) == 0 ? 'X - Win!' : '0 - Win!');
+        if ((complet % 2) == 0) {
+            nrJocX += 1;
+        }
+        else {
+            nrJoc0 += 1;
+        }
+
+        $('.winNrX').text(nrJocX);
+        $('.winNr0').text(nrJoc0);
+    }
     if ((complet == 0) && (win == 0)) {
         $('.remiza').addClass('visible');
-        
     }
-
 }
+
+
+
 $('.buttonRemiza').click(replay);
 
 function replay() {
