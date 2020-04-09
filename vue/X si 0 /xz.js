43aec127c433395baa7846c136ecd2d6b6d2761a
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-09 20:08:38 +0300

    X&0 - Vue

--- vue/X si 0 /xz.js
+++ vue/X si 0 /xz.js
@@ -1,26 +1,30 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        table: [
+            '', '', '',
+            '', '', '',
+            '', '', '',
+        ],
+        finished: false
+    },
+    methods: {
+        makeMove() {
+            
+        }
+    }
+});
+
+
 var activePlayer = 'X';
-var win = 0;
-var complet = 9;
-
-var nrJocX = 0;
-var nrJoc0 = 0;
-
-$('.winNrX').text('0');
-$('.winNr0').text('0');
-
 $('.table div').click(makeMove);
 
 function makeMove() {
-    if (win == 0) {
-        complet = complet - 1;
-        $(this).text(activePlayer)
-            .addClass(activePlayer == 'X' ? 'x' : 'z')
-            .unbind('click', makeMove);
-
-        activePlayer = (activePlayer == 'X') ? '0' : 'X';
-        checkEndGame();
-
-    }
+    $(this).text(activePlayer)
+           .addClass(activePlayer == 'X' ? 'x' : 'z')
+           .unbind('click', makeMove);
+    activePlayer = activePlayer == 'X' ? '0' : 'X';
+    checkEndGame();
 }
 
 function checkEndGame() {
@@ -28,15 +32,12 @@ function checkEndGame() {
     checkComplete(cells.filter((value, index) => index < 3));
     checkComplete(cells.filter((value, index) => index > 2 && index < 6));
     checkComplete(cells.filter((value, index) => index > 5));
-
     checkComplete(cells.filter((value, index) => index % 3 == 0));
     checkComplete(cells.filter((value, index) => index % 3 == 1));
     checkComplete(cells.filter((value, index) => index % 3 == 2));
-
+    checkComplete(cells.filter((value, index) => index % 3 == 2));
     checkComplete(cells.filter((value, index) => index % 4 == 0));
-
     checkComplete(cells.filter((value, index) => [2, 4, 6].includes(index)));
-    finish();
 }
 
 /**
@@ -44,46 +45,9 @@ function checkEndGame() {
  * @param {Array} cells 
  */
 function checkComplete(cells) {
-    if (cells.every(cell => $(cell).text() == 'X')
-        || cells.every(cell => $(cell).text() == '0') && (complet != 0)
+    if(cells.every(cell => $(cell).text() == 'X') 
+    || cells.every(cell => $(cell).text() == '0')   
     ) {
-        win = 1;
+        $('.end').addClass('visible');
     }
-}
-
-function finish() {
-    if (win == 1) {
-        $('.end').addClass('visible').text((complet % 2) == 0 ? 'X - Win!' : '0 - Win!');
-        if ((complet % 2) == 0) {
-            nrJocX += 1;
-        }
-        else {
-            nrJoc0 += 1;
-        }
-
-        $('.winNrX').text(nrJocX);
-        $('.winNr0').text(nrJoc0);
-    }
-    if ((complet == 0) && (win == 0)) {
-        $('.remiza').addClass('visible');
-    }
-}
-
-
-
-$('.buttonRemiza').click(replay);
-
-function replay() {
-    activePlayer = 'X';
-    win = 0;
-    complet = 9;
-
-    $('.end').removeClass('visible');
-    $('.remiza').removeClass('visible');
-
-
-    $('.table div').text('')
-        .removeAttr('class')
-        .unbind('click', makeMove)
-        .bind('click', makeMove);
 }
