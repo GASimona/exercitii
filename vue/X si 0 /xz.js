bc6891779beef2ea1f65bbf46404326916a3a1ac
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-10 12:53:32 +0300

    x&0 cu Vue

--- vue/X si 0 /xz.js
+++ vue/X si 0 /xz.js
@@ -1,53 +1,43 @@
 var app = new Vue({
-    el:'#app',
-    data:{
+    el: '#app',
+    data: {
         table: [
             '', '', '',
             '', '', '',
             '', '', '',
         ],
+        activePlayer: 'X',
         finished: false
     },
     methods: {
-        makeMove() {
-            
+        // "index" reprezinta celula din tabel pe care se face mutarea(0-8)
+        makeMove(index) {
+            // (guard clause) verificam daca este deja ceva pe acea celula
+            if (this.table[index] != '') return;
+            // folosim "splice" pt a permite reactivitatea proprietatii table
+            this.table.splice(index, 1, this.activePlayer);
+            // schimbam jucatorul activ
+            this.activePlayer = this.activePlayer == 'X' ? '0' : 'X';
+            // verificam daca s-a terminat jocul
+            this.checkFinished();
+        },
+        checkFinished() {
+            this.checkComplete(this.table.filter((value, index) => index < 3));
+            this.checkComplete(this.table.filter((value, index) => index > 2 && index < 6));
+            this.checkComplete(this.table.filter((value, index) => index > 5));
+            this.checkComplete(this.table.filter((value, index) => index % 3 == 0));
+            this.checkComplete(this.table.filter((value, index) => index % 3 == 1));
+            this.checkComplete(this.table.filter((value, index) => index % 3 == 2));
+            this.checkComplete(this.table.filter((value, index) => index % 3 == 2));
+            this.checkComplete(this.table.filter((value, index) => index % 4 == 0));
+            this.checkComplete(this.table.filter((value, index) => [2, 4, 6].includes(index)));
+        },
+        checkComplete(cells) {
+            if (cells.every(cell => cell == 'X')
+                || cells.every(cell => cell == '0')
+            ) {
+                this.finished = true;
+            }
         }
     }
 });
-
-
-var activePlayer = 'X';
-$('.table div').click(makeMove);
-
-function makeMove() {
-    $(this).text(activePlayer)
-           .addClass(activePlayer == 'X' ? 'x' : 'z')
-           .unbind('click', makeMove);
-    activePlayer = activePlayer == 'X' ? '0' : 'X';
-    checkEndGame();
-}
-
-function checkEndGame() {
-    let cells = $('.table div').toArray();
-    checkComplete(cells.filter((value, index) => index < 3));
-    checkComplete(cells.filter((value, index) => index > 2 && index < 6));
-    checkComplete(cells.filter((value, index) => index > 5));
-    checkComplete(cells.filter((value, index) => index % 3 == 0));
-    checkComplete(cells.filter((value, index) => index % 3 == 1));
-    checkComplete(cells.filter((value, index) => index % 3 == 2));
-    checkComplete(cells.filter((value, index) => index % 3 == 2));
-    checkComplete(cells.filter((value, index) => index % 4 == 0));
-    checkComplete(cells.filter((value, index) => [2, 4, 6].includes(index)));
-}
-
-/**
- * 
- * @param {Array} cells 
- */
-function checkComplete(cells) {
-    if(cells.every(cell => $(cell).text() == 'X') 
-    || cells.every(cell => $(cell).text() == '0')   
-    ) {
-        $('.end').addClass('visible');
-    }
-}
