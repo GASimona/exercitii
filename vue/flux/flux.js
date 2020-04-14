4118dcf7425e8d96763dae13401d39f9ec338d91
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-14 20:32:14 +0300

    v-for

--- vue/flux/flux.js
+++ vue/flux/flux.js
@@ -1,12 +1,22 @@
 var app = new Vue({
     el: '#app',
-    data:{
-        good: false
+    data: {
+        good: false,
+        todo: [
+            'Stay at home',
+            'Be safe',
+            'Learn Vue.js'
+        ]
     },
     methods: {
         hour() {
             let now = new Date();
             return now.getHours();
+        },
+        addNewItem() {
+            this.todo.splice(2, 1, 'Test arrays.');
+            // this.todo.shift();
+            // this.todo.pop();
         }
     }
 })
