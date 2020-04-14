bf77903f26a45e492b28f022de064ed5dcecb5b5
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-14 19:39:06 +0300

    reversedMessage

--- vue/mustache/mustach.js
+++ vue/mustache/mustach.js
@@ -1,8 +1,18 @@
-var app =new Vue ({
-    el:'#app',
+var app = new Vue({
+    el: '#app',
     data: {
         message: 'Do you like my moustache? }}',
         counter: 10,
         myclass: 'important'
+    },
+    method: {
+    },
+    computed: {
+        reversedMessage() {
+            return this.message
+                .split('')
+                .reverse()
+                .join('');
+        }
     }
 })
