bdea7b2ffcb1e8dd5248dac727ff487f66ab6301
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-20 14:04:48 +0300

    add Ashe (hero)

--- oop/polimorfism/heroes/heroes.js
+++ oop/polimorfism/heroes/heroes.js
@@ -41,15 +41,44 @@ class Mage extends Hero {
     }
 }
 
+class Marksman extends Hero {
+    firePrimary() {
+        this.mana -= 3;
+        this.life += 5;
+        if (this.life > 500) {
+            this.life = 500;
+        }
+    }
+    fireSecondary() {
+        this.mana -= 0;
+        this.life += 5;
+        if (this.life > 500) {
+            this.life = 500;
+        }
+    }
+    receiveHit() {
+        this.life -= 10;
+    }
+}
+
 var clark = new Superman("Clark Kent", 1000, 1000);
 var xevras = new Mage("Xevras", 100, 300);
+var ashe = new Marksman("Ashe", 500, 200);
 
 
 clark.firePrimary();
 xevras.receiveHit();
+ashe.firePrimary();
+clark.receiveHit();
+ashe.firePrimary();
+clark.receiveHit();
+ashe.firePrimary();
+clark.receiveHit();
 xevras.firePrimary();
 clark.receiveHit();
 xevras.fireSecondary();
+clark.receiveHit();
+ashe.firePrimary();
 clark.receiveHit();
 clark.fireSecondary();
 xevras.receiveHit();
@@ -57,6 +86,8 @@ xevras.receiveHit();
 
 console.log(clark);
 console.log(xevras);
+console.log(ashe);
 
 console.log(clark.isAlive());
 console.log(xevras.isAlive());
+console.log(ashe.isAlive());
