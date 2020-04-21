724e2ebba44a8dd3b010b834f7020e9d9730792a
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-21 17:05:06 +0300

    polimorfism

--- oop/polimorfism/heroes/heroes.js
+++ oop/polimorfism/heroes/heroes.js
@@ -7,87 +7,82 @@ class Hero {
         this.mana = mana;
     }
     // metode
-    firePrimary() { }
-    fireSecondary() { }
-    receiveHit() { }
+    firePrimary(anotherHero) { }
+    fireSecondary(anotherHero) { }
+    receiveHit(energy) { }
     isAlive() {
         return this.life > 0;
     }
 }
 
 class Superman extends Hero {
-    firePrimary() {
+    firePrimary(anotherHero) {
         this.mana -= 5;
+        anotherHero.receiveHit(30);
     }
-    fireSecondary() {
+    fireSecondary(anotherHero) {
         this.mana -= 1;
+        anotherHero.receiveHit(5);
     }
-    receiveHit() {
-        this.life -= 0.5;
+    receiveHit(energy) {
+        if (energy > 10) {
+            this.life -= 0.1 * energy;
+        }
     }
 }
 
 class Mage extends Hero {
-    firePrimary() {
+    firePrimary(anotherHero) {
         this.mana -= 20;
         this.life += 2;
+        anotherHero.receiveHit(15);
     }
-    fireSecondary() {
+    fireSecondary(anotherHero) {
         this.mana -= 10;
         this.life += 1;
+        anotherHero.receiveHit(5);
     }
-    receiveHit() {
+    receiveHit(energy) {
         this.life -= 10;
+        this.mana += 0.5 * energy;
     }
 }
 
-class Marksman extends Hero {
-    firePrimary() {
-        this.mana -= 3;
-        this.life += 5;
-        if (this.life > 500) {
-            this.life = 500;
-        }
-    }
-    fireSecondary() {
-        this.mana -= 0;
-        this.life += 5;
-        if (this.life > 500) {
-            this.life = 500;
-        }
-    }
-    receiveHit() {
-        this.life -= 10;
-    }
-}
+// class Marksman extends Hero {
+//     firePrimary() {
+//         this.mana -= 3;
+//         this.life += 5;
+//         if (this.life > 500) {
+//             this.life = 500;
+//         }
+//     }
+//     fireSecondary() {
+//         this.mana -= 0;
+//         this.life += 5;
+//         if (this.life > 500) {
+//             this.life = 500;
+//         }
+//     }
+//     receiveHit() {
+//         this.life -= 10;
+//     }
+// }
 
 var clark = new Superman("Clark Kent", 1000, 1000);
 var xevras = new Mage("Xevras", 100, 300);
-var ashe = new Marksman("Ashe", 500, 200);
+// var ashe = new Marksman("Ashe", 500, 200);
 
 
-clark.firePrimary();
-xevras.receiveHit();
-ashe.firePrimary();
-clark.receiveHit();
-ashe.firePrimary();
-clark.receiveHit();
-ashe.firePrimary();
-clark.receiveHit();
-xevras.firePrimary();
-clark.receiveHit();
-xevras.fireSecondary();
-clark.receiveHit();
-ashe.firePrimary();
-clark.receiveHit();
-clark.fireSecondary();
-xevras.receiveHit();
+clark.firePrimary(xevras);
+xevras.firePrimary(clark);
+xevras.fireSecondary(clark);
+clark.fireSecondary(xevras);
 
 
 console.log(clark);
 console.log(xevras);
-console.log(ashe);
+// console.log(ashe);
 
 console.log(clark.isAlive());
 console.log(xevras.isAlive());
-console.log(ashe.isAlive());
+// console.log(ashe.isAlive());
