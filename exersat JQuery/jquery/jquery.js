8b29e1dddf1daeb5c3f3ae722547722907b21096
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-22 19:18:53 +0300

    .

---
+++ exersat JQuery/jquery/jquery.js
@@ -0,0 +1,28 @@
+// $('.b').hide();
+
+// var buri = document.getElementsByClassName('b');
+
+// for (i = 0; i < buri.length; i++) {
+//     var ultimulB = buri[i];
+//     ultimulB.style.display = 'none';
+// }
+
+
+var newCopil = $('<div></div>').addClass('b').text(3);
+$('.a').append(newCopil);
+// $('.a').prepend(newCopil);
+// $('.a').after(newCopil);
+// $('.a').before(newCopil);
+
+// $('.b:nth-child(2)').removeClass('b');
+
+// var create = document.createElement('div');
+// create.className = 'b'; 
+// create.textContent = 5;
+// var listaElementeClassA = document.getElementsByClassName('a');
+// var primulElementDinClasaA = listaElementeClassA[0];
+// primulElementDinClasaA.appendChild(create);
+
+// primulElementDinClasaA.parentElement.append(create);
+
+// $('<div></div>').addClass('b').text(4).insertAfter('.a') ;
