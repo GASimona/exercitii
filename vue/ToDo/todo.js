5c2fd8497ad784bd814e8e3634a5256c07669993
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-23 17:05:38 +0300

    Vue.component('todo-list')

--- vue/ToDo/todo.js
+++ vue/ToDo/todo.js
@@ -21,15 +21,41 @@ Vue.component('task', {
     props: ['title', 'isDone']
 });
 
-
-
-var app = new Vue({
-    el: "#app",
-    data: {
-        todos: [
-            new Todo('Something to do')
-        ],
-        task: ''
+Vue.component('todo-list', {
+    template: `
+            <div class="todo-list">
+                <h1>To Do:</h1>
+                <div class="filters">
+                    <label>Show done: </label>
+                    <input type="checkbox" v-model="showDone">
+                </div>
+                <ul>
+                    <task 
+                        v-if="showDone || !todo.isDone"
+                        v-for="(todo, index) in todos" 
+                        :title="todo.title" 
+                        :is-done="todo.isDone"
+                        @checked="checkTodo(index)"
+                        @deleted="deletedTodo(index)">
+                    </task>
+                </ul>
+                <div class="new_task">
+                    Task
+                    <input type="text" v-model="task" @keyup.enter="saveTodo"></input>
+                    <div class="save_task">
+                        <button @click="saveTodo">Save To Do</button>
+                    </div>
+                </div>
+            </div>
+            `,
+    data() {
+        return {
+            todos: [
+                new Todo('Something to do')
+            ],
+            task: '',
+            showDone: true
+        }
     },
     methods: {
         saveTodo() {
@@ -45,4 +71,8 @@ var app = new Vue({
             this.todos.splice(index, 1);
         }
     }
+});
+
+var app = new Vue({
+    el: "#app",
 });
