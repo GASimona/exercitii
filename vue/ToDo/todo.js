231c5e87f9cb62ba2cea091ff23c514e9d3c00ae
Simona Gorneanu <gorneanusimona@gmail.com>
2020-04-23 16:39:33 +0300

    done & delete

--- vue/ToDo/todo.js
+++ vue/ToDo/todo.js
@@ -1,15 +1,48 @@
+class Todo {
+    constructor(title) {
+        this.title = title;
+        this.isDone = false;
+    }
+    switchDone() {
+        this.isDone = !this.isDone;
+    }
+}
+
+Vue.component('task', {
+    template: `
+              <li :class="{ done: isDone }" class="task">
+                  <div class="title">{{ title }}</div>
+                  <div class="controls">
+                     <div class="check" @click="$emit('checked')">✓</div>
+                     <div class="delete" @click="$emit('deleted')">✗</div>
+                 </div>
+              </li>
+              `,
+    props: ['title', 'isDone']
+});
+
+
+
 var app = new Vue({
     el: "#app",
     data: {
-        todos: [],
+        todos: [
+            new Todo('Something to do')
+        ],
         task: ''
     },
     methods: {
-        saveItem() {
+        saveTodo() {
             // -------- adaugam valoarea din task in todos -------------------
-            this.todos.push(this.task);
+            this.todos.push(new Todo(this.task));
             // -------- resetam task-ul -------------------
             this.task = '';
+        },
+        checkTodo(index) {
+            this.todos[index].switchDone();
+        },
+        deletedTodo(index) {
+            this.todos.splice(index, 1);
         }
     }
 });
