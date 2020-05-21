6628a003c47926bbd6c2815ec83fd0d130c8bc4b
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-05-21 13:14:56 +0300

    start react

--- react/hello/hello.js
+++ react/hello/hello.js
@@ -1,23 +1,30 @@
-const name = "Simona";
-const uri = 'https://reactjs.org/';
-const sayHello = true;
+class Welcome extends React.Component {
 
-const elem = (
-    <h1>
-        {/* { sayHello && <div>Hello, { name.toUpperCase() }!</div> }  */}
-        {/* aici este un if / else */}
-        { sayHello  
-            ? <div>Hello, { name.toUpperCase() }!</div>
-            : <div>Good bye!</div> 
+render() {
+    return (
+            <h1>
+                {/* { sayHello && <div>Hello, { name.toUpperCase() }!</div> }  */}
+                {/* aici este un if / else */}
+                { this.params.sayHello  
+                    ? <div>Hello, { this.params.name.toUpperCase() }!</div>
+                    : <div>Good bye!</div> 
+                }
+                <div>{ new Date().toDateString() } </div>
+                <div>{ new Date().getHours() } : { new Date().getMinutes() } : { new Date().getSeconds() } </div>
+                <div> { new Date().toISOString() } </div>
+                <a href={ this.params.uri } className="highlight">Learn more about React</a>
+            </h1>
+            )
         }
+}
 
-        <div>{ new Date().toDateString() } </div>
-        <a href={ uri }>Learn more about React</a>
-    </h1>
-)
+const App = function() {
+    return (
+        <Welcome sayHello={true} uri="https://reactjs.org/" name= "Simona"/>
+    )
+}
 
 ReactDOM.render(
-    elem,
+    <App/>,
     document.getElementById('root')
 )
-
