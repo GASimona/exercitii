6d8f8333d5cb1ba08b8d333abefaf9407c699262
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-05-21 20:09:00 +0300

    vuex todo

--- vue/todo-redesign/js/app.js
+++ vue/todo-redesign/js/app.js
@@ -1,7 +1,13 @@
-import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
+// import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
+// import 'https://cdn.jsdelivr.net/npm/vuex/dist/vuex.js';
+
+import 'https://unpkg.com/vue/dist/vue.js';
+import 'https://unpkg.com/vuex/dist/vuex.js';
 import './component/task.js';
 import './component/todo-list.js';
+import store from './store/store.js';
 
 var app = new Vue({
     el: "#app",
+    store
 });
--- vue/todo-redesign/js/component/todo-list.js
+++ vue/todo-redesign/js/component/todo-list.js
@@ -1,4 +1,5 @@
-import { Todo } from '../class/todo.js';
+import Todo from '../class/todo.js';
+import store from '../store/store.js';
 
 Vue.component('todo-list', {
     template: `
@@ -33,23 +34,40 @@ Vue.component('todo-list', {
             todos: [
                 new Todo('Something to do')
             ],
-            task: '',
-            showDone: true
+            task: ''
+            // showDone: true
         }
     },
     props: [ 'list_name' ],
     methods: {
         saveTodo() {
             // -------- adaugam valoarea din task in todos -------------------
-            this.todos.push(new Todo(this.task));
+            // this.todos.push(new Todo(this.task));
+            this.$store.comit('saveTodo', this.task)
             // -------- resetam task-ul -------------------
             this.task = '';
         },
         checkTodo(index) {
-            this.todos[index].switchDone();
+            // this.todos[index].switchDone();
+            this.$store.comit('checkTodo', index)
         },
         deletedTodo(index) {
-            this.todos.splice(index, 1);
+            // this.todos.splice(index, 1);
+            this.$store.comit(deletTodo, index)
+        },
+        changeShowDone: (state, value) => state.showDone = value
+    },
+    computed: {
+        filteredTodos() {
+            return store.getters.filteredTodos
+        },
+        showDone: {
+            get() {
+                return this$store.state.showDone
+            },
+            set(value) {
+                this.$store.comit('changeShowDone', value)
+            }
         }
     }
 });
