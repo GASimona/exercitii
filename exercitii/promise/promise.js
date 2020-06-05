9699e5bc3af3809b1e7fc8844c338d76c4bc9cac
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-06-05 14:03:48 +0300

    callback cu promisiuni

---
+++ exercitii/promise/promise.js
@@ -0,0 +1,33 @@
+
+//-----varianta 1-----------------
+// myPromise.then(
+//     result => alert(result),
+//     error => alert(error)
+// )
+
+// -----varianta 2---------------
+// myPromise.then(result => alert(result))
+//         .catch(error => alert(error));
+
+
+new Promise((resolve, reject) => {
+            setTimeout( () => resolve('unu'), 3000) 
+        }).then(result => {
+            addDiv(result);
+            return new Promise((resolve, reject) => {
+                setTimeout( () => resolve('doi'), 2000);
+            });
+        }).then(result => {
+             addDiv(result);
+             return new Promise((resolve, reject) => {
+                setTimeout( () => resolve('trei'), 1000);
+            });
+        }).then(result => {
+             addDiv(result);
+        });         
+
+function addDiv(text) {
+    let div = document.createElement('div');
+    div.innerText = text;
+    document.querySelector('body').appendChild(div);
+}
