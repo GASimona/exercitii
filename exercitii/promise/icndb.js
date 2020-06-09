8a24cba911bd223e544960243853f031a3d9f439
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-06-09 21:04:13 +0300

    promise

--- exercitii/promise/icndb.js
+++ exercitii/promise/icndb.js
@@ -2,12 +2,25 @@
 
 axios.get('http://api.icndb.com/jokes/random')
     .then(
-        response => {
-            document.querySelector('div:nth-child(1)').innerText = response.data.value.joke
+        (response) => {
+            document.querySelector('div:nth-child(1)').innerText = response.data.value.joke;
+            console.log(response);
             return axios.get('http://api.icndb.com/jokes/random')
         }
     )
-    .then( response => { document.querySelector('div:nth-child(2)').innerText = response.data.value.joke })
+    .then( 
+        (response) => { 
+            document.querySelector('div:nth-child(2)').innerText = response.data.value.joke; 
+            console.log(response);
+            return axios.get('http://api.icndb.com/jokes/random')
+        }
+    )
+    .then( 
+        (response) => { 
+            document.querySelector('div:nth-child(3)').innerText = response.data.value.joke; 
+            console.log(response);
+        }
+    )
     .catch(
-        error => console.log('error')
+        (error) => console.log('error')
     )
