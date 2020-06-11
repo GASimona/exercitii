fefea6df8a5bbe494277c142c33b64e3d14be650
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-06-11 19:27:17 +0300

    hanoi rod & disks

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -1,5 +1,15 @@
+import 'http://unpkg.com/vue/dist/vue.js'
+import 'http://unpkg.com/vuex/dist/vuex.js'
+
 function randomColor() {
-    return '#' + Math.random().toString(16).substr(-6)
+    return '#' + Math.random().toString(16).substr(-6);
+}
+
+class Move {
+    constructor(sourceRodId, targetRodId) {
+        this.sourceRodId = sourceRodId
+        this.targetRodId = targetRodId
+    }
 }
 
 class Disk {
@@ -7,40 +17,40 @@ class Disk {
         this.size = size
         this.color = randomColor()
     }
-    biggerThan(anotherDisks) {
-        return this.size >anotherDisks.size;
+    biggerThan(anotherDisk) {
+        return this.size >anotherDisk.size;
     }
 }
 
 class Rod {
-    constructor(id, numberOfDisk, size) {
+    constructor(id, numberOfDisks, size) {
         this.id = id
         this.size = size
-        this.disk = []
-        createDisks(numberOfDisks)
-
+        this.disks = []
+        this.createDisks(numberOfDisks)
     }
     createDisks(numberOfDisks) {
-        for(let size=numberOfDisk; size>0; size--) {
-            this.createDisks.push(new Disk(size))
+        for(let size=numberOfDisks; size>0; size--) {
+            this.disks.push(new Disk(size))
         }
     }
     put(disk) {
         if (!this.canPutDisk(disk)) throw new Error('Cannot put disk')
-        this.createDisks.push(disk);
+        this.disks.push(disk);
     }
     canPutDisk(disk) {
-        return this.isEmpty() || this.topDisk().bigerThan(disk)
+        return this.isEmpty() 
+            || this.topDisk().biggerThan(disk)
     }
     pop() {
         if (this.isEmpty()) throw new Error('Rod is empty')
-        return this.createDisks.pop()
+        return this.disks.pop()
     }
     isEmpty() {
         return this.disks.length == 0
     }
     topDisk() {
-        return this.disks[this.disk.length - 1]
+        return this.disks[this.disks.length - 1]
     }
 }
 
@@ -54,9 +64,9 @@ class Game {
       
         this.numberOfMoves = 0
     }
-    move(sourceRodId, targetRodId) {
-        let sourceRod = this.rods[sourceRodId]
-        let targetRod = this.rods[targetRodId]
+    makeMove(move) {
+        let sourceRod = this.rods[move.sourceRodId]
+        let targetRod = this.rods[move.targetRodId]
 
         if(!targetRod.canPutDisk(sourceRod.topDosk())) {
             throw new Error('Cannot move disk')
@@ -65,3 +75,61 @@ class Game {
         targetRod.put(disk)
     }
 }
+
+const store = new Vuex.Store({
+    state: {
+        game: new Game(4)
+    },
+    mutations: {
+        makeMove: (state, move) => state.game.makeMove(move)
+    },
+    getters: {
+        game: (state) => state.game
+    }
+})
+
+const rod = {
+    props: ['rod'],
+    template: `
+        <div class="rod"
+             :style="{ height: disksHeight(rod.size),
+                       width: diskWidth(rod.size)
+              }"    
+        >
+            <div v-for="disk in rod.disks"
+                 :key="disk.size"
+                 class="disk"
+                 :style="{ 
+                     height: disksHeight(1),
+                     width: diskWidth(disk.size),
+                     backgroundColor: disk.color
+                  }"
+            >
+
+            </div>
+        </div>
+    `,
+    methods: {
+        disksHeight: (disks) => disks * 2 + 'em',
+        diskWidth: (diskSize) => diskSize * 3 + 'em' 
+    },
+}
+
+const game =  {
+    template: `
+        <div class="game">
+            <rod v-for="rod in rods" :rod="rod" :key="rod.id"></rod>
+        </div>
+    `,
+    computed: {
+        rods: () => store.state.game.rods
+    },
+    components: {rod}
+}
+
+new Vue({
+    el:'#app',
+    store,
+    components:{rod, game},
+    template: `<game></game>`
+})
