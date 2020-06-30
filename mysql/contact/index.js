de120298adf81d5084ff6fcf232edb08488f4eae
Simona Gorneanu <simona@Simonas-MacBook-Pro.local>
2020-06-30 20:59:32 +0300

    sql - js

---
+++ mysql/contact/index.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql")
+const express = require("express")
+const bodyParse = require("body-parser")
+const port = 80
+const uri = `http://localhost:${port}`
+
+const app = express()
+app.use(bodyParse.urlencoded({extended: true}))
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "myuser",
+    password: "1234",
+    database: "gregs_list"
+})
+
+con.connect( (err) => {
+    if (err) throw err
+    console.log("Connected to MySQL")
+})
+
+// inregistreaza rutele
+app.post("/contact", (req, res) => {
+    console.log(req.body)
+    con.query(`INSERT INTO conctact VALUES (NULL, '${req.body.name}', '${req.body.email}', '${req.body.message}')`)
+    // dam raspuns
+    res.sendStatus(200)
+})
+
+// porneste aplicatia server
+app.listen(
+    port, () => console.log(`Server started on ${uri}`)
+)
